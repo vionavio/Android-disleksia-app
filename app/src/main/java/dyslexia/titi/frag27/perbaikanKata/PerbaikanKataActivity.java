2d47c0c3c78451d8ddb1619e290aa8771c6cb563
maryati-titi <titi.666198maryati@gmail.com>
2019-08-01 12:04:28 +0700

    login

---
+++ app/src/main/java/dyslexia/titi/frag27/perbaikanKata/PerbaikanKataActivity.java
@@ -0,0 +1,14 @@
+package dyslexia.titi.frag27.perbaikanKata;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PerbaikanKataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perbaikan_kata);
+    }
+}
