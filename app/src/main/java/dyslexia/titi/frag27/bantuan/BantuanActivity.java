052b86ae4670364cc2f9fe874af4f26c45dd645a
maryati-titi <titi.666198maryati@gmail.com>
2019-08-01 13:00:18 +0700

    bantuan

---
+++ app/src/main/java/dyslexia/titi/frag27/bantuan/BantuanActivity.java
@@ -0,0 +1,14 @@
+package dyslexia.titi.frag27.bantuan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BantuanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bantuan);
+    }
+}
