67f77c5a333e396c30abe96d17ac78937dce881d
arifikhsan <arif.ikhsanudin.developer@gmail.com>
2019-10-07 10:27:50 +0700

    add utils forlder and constant store

---
+++ app/src/main/java/dyslexia/titi/frag27/utils/Constant.java
@@ -0,0 +1,11 @@
+package dyslexia.titi.frag27.utils;
+// Created by Arif Ikhsanudin on 10/7/2019.
+
+public class Constant {
+    static final String IS_LOGGED_IN = "isLoggedIn";
+    static final String USER_ID = "userId";
+    static final String TAG = "aaaaaa";
+    public static final String GAME_NUMBER = "gameNumber";
+
+    public static final String DATABASE_NAME = "disleksia_db";
+}
