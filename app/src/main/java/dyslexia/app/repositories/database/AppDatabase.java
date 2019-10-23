01040f6b99a3849c63c6c6af0d5962255e14ceee
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2019-10-23 10:24:47 +0700

    up schema version number

--- app/src/main/java/dyslexia/app/repositories/database/AppDatabase.java
+++ app/src/main/java/dyslexia/app/repositories/database/AppDatabase.java
@@ -17,7 +17,7 @@ import dyslexia.app.repositories.database.entities.WordEntity;
 
 import static dyslexia.app.utils.Constant.DATABASE_NAME;
 
-@Database(entities = {UserEntity.class, ScoreEntity.class, WordEntity.class, SpeechEntity.class}, version = 2)
+@Database(entities = {UserEntity.class, ScoreEntity.class, WordEntity.class, SpeechEntity.class}, version = 3)
 public abstract class AppDatabase extends RoomDatabase {
     private static AppDatabase appDatabaseInstance;
 
